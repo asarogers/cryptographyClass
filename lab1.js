82e3c1e70a2aebeaf7e78abd99e69f3a7b2d3922
Asa Rogers <cyberasasoftware@gmail.co>
2023-03-27 09:41:51 -0500

    first

---
+++ lab1.js
@@ -0,0 +1,23 @@
+function encrypt(text) {
+    var encryptedWord = "";
+    var char = '';
+
+    for (let ch of text) {
+        char = (ch.charCodeAt(0) + 6);
+        encryptedWord += String.fromCharCode(char);
+    }
+    return encryptedWord;
+}
+
+function decrypt(text) {
+    var decrypted = "";
+    var char = '';
+
+    for (let ch of text) {
+        char = (ch.charCodeAt(0) - 6);
+        decrypted += String.fromCharCode(char);
+    }
+    return (decrypted)
+}
+
+console.log(encrypt("Party at five"));
