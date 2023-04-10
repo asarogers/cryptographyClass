4f039c57aff54d44e40a6e21dfe2801485c80ffa
Olasubomi Olawepo <olasubomiolawepo@Olasubomis-MacBook-Pro-2.local>
2023-04-10 13:53:50 -0500

    update

--- lab2.js
+++ lab2.js
@@ -27,16 +27,17 @@ const substitutionTable = {
     'Y': 'B',
     'Z': 'A'
   };
-  
+  const plaintext = 'BULLDOGS';
+
   // The permutation table for the algorithm
   const permutationTable = [3, 1, 4, 2];
   
   // The number of rounds to use
-  const numRounds = 3;
+  const numRounds = 5;
   
   // The block size to use
-  const blockSize = 4;
-  
+  const blockSize =1;
+  console.log(blockSize)
   // Encrypt a plaintext message
   function encrypt(plaintext) {
     // Divide the plaintext into blocks of size `blockSize`
@@ -47,15 +48,15 @@ const substitutionTable = {
       let ciphertextBlock = block;
   
       for (let round = 0; round < numRounds; round++) {
-        // Apply substitution
-        ciphertextBlock = ciphertextBlock.split('').map(symbol => substitutionTable[symbol]).join('');
-  
         // Apply permutation
         const permutedBlock = [];
         for (let i = 0; i < permutationTable.length; i++) {
-          permutedBlock[i] = ciphertextBlock[permutationTable[i] - 1];
+          permutedBlock[permutationTable[i] - 1] = ciphertextBlock[i];
         }
         ciphertextBlock = permutedBlock.join('');
+  
+        // Apply substitution
+        ciphertextBlock = ciphertextBlock.split('').map(symbol => substitutionTable[symbol]).join('');
       }
   
       return ciphertextBlock;
@@ -75,42 +76,33 @@ const substitutionTable = {
       let plaintextBlock = block;
   
       for (let round = 0; round < numRounds; round++) {
+        // Apply substitution in reverse order
+        plaintextBlock = plaintextBlock.split('').map(symbol => {
+          for (const key in substitutionTable) {
+            if (substitutionTable[key] === symbol) {
+              return key;
+            }
+          }
+          return symbol;
+        }).join('');
+  
         // Apply permutation in reverse order
         const permutedBlock = [];
         for (let i = 0; i < permutationTable.length; i++) {
-          permutedBlock[permutationTable[i] - 1] = plaintextBlock[i];
+          permutedBlock[i] = plaintextBlock[permutationTable[i] - 1];
         }
         plaintextBlock = permutedBlock.join('');
+      }
   
-        // Apply substitution in reverse order
-        plaintextBlock = plaintextBlock.split('').map(symbol => {
-            for (const key in substitutionTable) {
-              if (substitutionTable[key] === symbol) {
-                return key;
-              }
-            }
-            return symbol;
-          }).join('');
-        }
-        
-        return plaintextBlock;
-        });
-        
-        // Return the plaintext as a single string
-        return plaintextBlocks.join('');
-        }
-        
-        // Example usage
-        const plaintext = 'HELLO WORLD';
-        const ciphertext = encrypt(plaintext);
-        console.log(`Ciphertext: ${ciphertext}`);
-        console.log(`Plaintext: ${decrypt(ciphertext)}`);
-        
-        // Output:
-        // Ciphertext: ZMHXJLUVGFA
-        
-        
-        
-        
-        
+      return plaintextBlock;
+    });
+  
+    // Return the plaintext as a single string
+    return plaintextBlocks.join('');
+  }
+  
+  // Example usage
+  const ciphertext = encrypt(plaintext);
+  console.log(`Ciphertext: ${ciphertext}`);
+  console.log(`Plaintext: ${decrypt(ciphertext)}`);
   
